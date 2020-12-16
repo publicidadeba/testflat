9b14239d13cd097e3565ac276992e759560e17e4
Ernani Rocha <publicidadeba@gmail.com>
2020-12-16 17:15:29 -0300

    Major push
    - main level working
    - BG
    - jump fixed
    - player values tuned

---
+++ ProjectFlat/Assets/Script/CamFollow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour
+{
+
+    public Transform target;
+
+    //new code
+    public Vector3 offset;
+    [Range(1,10)]
+    public float smoothFactor;
+    public Vector3 minValues, maxValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(target.transform.position.x, target.transform.position.y,target.transform.position.z);
+    }
+
+    void Follow(){
+        Vector3 targetPosition = target.position + offset;
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, targetPosition, smoothFactor*Time.fixedDeltaTime);
+        transform.position = targetPosition;;
+    }
+}
---
+++ ProjectFlat/Assets/Script/MovePro.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePro : MonoBehaviour
+{
+    public float speed = 1;
+    public float jumpForce = 1;
+    public float moveInput;
+    private bool facingRight = true;
+
+    private bool isGrounded = true;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+        if(facingRight == false && moveInput > 0) {
+            Flip();
+        } else if(facingRight == true && moveInput < 0){
+            Flip();
+        }
+
+        /*if(|Mathf.Approximately(0, Speed)) 
+        {
+            transform.rotation = movement > 0 ? Quaternion.Euler(0.180.0) : Quaternion.identity;
+        }
+        */
+
+
+
+
+
+    }
+
+
+    void Update (){
+
+        if(Input.GetButtonDown("Jump") && isGrounded == true) 
+        {
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            
+        }
+
+    }
+
+    void Flip(){
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+
+    }
+}
